bf410177f9a54f0568dde29bb896d909d4c2b5af
Welton Ferreira <welton.ferreira@crptecnologia.com.br>
2017-12-06 18:20:34 -0300

    refatoração dos pacotes

---
+++ app/src/main/java/com/welton/pongExtreme/Game/CenaGame.kt
@@ -0,0 +1,27 @@
+package com.welton.pongExtreme.Game
+
+import com.silvano.AndGraph.AGGameManager
+import com.silvano.AndGraph.AGScene
+
+
+class CenaGame(manager: AGGameManager) : AGScene(manager) {
+
+
+    override fun init() {
+        setSceneBackgroundColor(1f, 1f, 1f)
+
+
+    }
+
+    override fun restart() {
+
+    }
+
+    override fun stop() {
+    
+    }
+
+    override fun loop() {
+
+    }
+}
---
+++ app/src/main/java/com/welton/pongExtreme/Game/Game.java
@@ -0,0 +1,15 @@
+//Aplication package
+package com.welton.pongExtreme.Game;
+
+//Used Packages
+import android.os.Bundle;
+import com.silvano.AndGraph.AGActivityGame;
+
+
+public class Game extends AGActivityGame
+{
+    public void onCreate(Bundle saved)
+    {
+        super.onCreate(saved);
+    }
+}
